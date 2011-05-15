514548a757373d76bcba2c8d201c17f08f3a249c
Darrin Eden <darrin.eden@gmail.com>
2011-05-15 15:52:45 -0700

    white space

--- jellyfish.erl
+++ jellyfish.erl
@@ -14,10 +14,13 @@ route_rest(Req) ->
     handle_r(Req:get(method), Req:resource([lowercase, urldecode]), Req).
 
 handle_r('GET', [], Req) -> Req:file("index.html");
+
 handle_r('POST', ["deploy"], Req) ->
     Req:ok([{"Content-Type", "text/plain"}], util:rand_hex(16));
+
 handle_r('GET', ["deploy", Id], Req) ->
     Req:ok([{"Content-Type", "text/plain"}], "~s", [Id]);
+
 handle_r(_, _, Req) ->
     Req:ok([{"Content-Type", "text/plain"}], "Page not found.").
 
