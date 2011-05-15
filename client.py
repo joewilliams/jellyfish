5739c52773a0cc17ed6e8865f72a017aca5fba46
Darrin Eden <darrin.eden@gmail.com>
2011-05-15 15:40:41 -0700

    client send an open event

--- client.py
+++ client.py
@@ -11,10 +11,14 @@ def on_error(ws, error):
 def on_close(ws):
     print "### closed ###"
 
+def on_open(ws):
+    ws.send("open")
+
 if __name__ == "__main__":
     websocket.enableTrace(False)
     ws = websocket.WebSocketApp("ws://localhost:8080/deploy/90ada634e59b775f4ff393d5631b1d3f",
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
+    ws.on_open = on_open
     ws.run_forever()
