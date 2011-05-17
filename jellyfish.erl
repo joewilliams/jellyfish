253e38a84180a8cebd4c9e6a7e730685511877e8
Joe Williams <joe@joetify.com>
2011-05-16 17:55:46 -0700

    clean up formatting and use _ rather than _Name

--- jellyfish.erl
+++ jellyfish.erl
@@ -1,6 +1,8 @@
 -module(jellyfish).
 -export([start/1, stop/0]).
 
+%% External API
+
 start(Port) ->
     dets:open_file(clients, [{type, bag}]),
     misultin:start_link([{port, Port},
@@ -10,35 +12,33 @@ start(Port) ->
 stop() -> dets:close(clients),
           misultin:stop().
 
+%% Internal API
+
 route_rest(Req) ->
     handle_r(Req:get(method), Req:resource([lowercase, urldecode]), Req).
 
 handle_r('GET', [], Req) -> Req:file("index.html");
-
 handle_r('POST', ["deploy"], Req) ->
     Req:ok([{"Content-Type", "text/plain"}], util:rand_hex(16));
-
 handle_r('POST', ["deploy", Id], Req) ->
     signal(Id),
     Req:ok([{"Content-Type", "text/plain"}], "OK");
-
 handle_r('GET', ["deploy", Id], Req) ->
     signal(Id),
     Req:ok([{"Content-Type", "text/plain"}], "OK");
-
 handle_r(_, _, Req) ->
     Req:ok([{"Content-Type", "text/plain"}], "Page not found.").
 
 handle_websocket(Ws) ->
     receive
-        {browser, Data} ->
+        {browser, _} ->
             dets:insert(clients, {util:last_token(Ws:get(path)),
                                   self()}),
             handle_websocket(Ws);
         {event} ->
             Ws:send("event"),
             handle_websocket(Ws);
-        _Ignore ->
+        _ ->
             handle_websocket(Ws)
     end.
 
